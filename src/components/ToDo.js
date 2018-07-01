4ce616a27a195c8072b4b667de92ddb9794fa2c7
RD <rd@Suryas-MacBook-Pro.local>
2018-07-01 02:44:00 -0500

    checkpoint-react-to-do-propsandstate: completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render(){
     return (
-      <li> Todo list </li>
+      <li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+       </li>
     );
   }
 }
